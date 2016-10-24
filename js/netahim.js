26d4d19be416763c91ad12531ae2e2cfd5d47301
Maya6315 <rottenbergmaya@gmail.com>
2016-10-24 19:20:59 +0300

    Update netahim.js

--- js/netahim.js
+++ js/netahim.js
@@ -32,13 +32,3 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
-		function init_map(){
-			var myOptions = {zoom:15,center:new google.maps.LatLng(32.071399,34.786576),mapTypeId: google.maps.MapTypeId.ROADMAP};
-			map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
-			marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(32.071399,34.786576)});
-			infowindow = new google.maps.InfoWindow({content:'<strong>נתחים</strong><br>אלוף קלמן מגן 3, תל אביב, ישראל<br>'});
-			google.maps.event.addListener(marker, 'click', function(){
-				infowindow.open(map,marker);});
-			infowindow.open(map,marker);}
-			google.maps.event.addDomListener(window, 'load', init_map);
